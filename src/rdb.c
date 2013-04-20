9704ed4daf30d0463e7070d101dfba0e526b71f2
Matt Palmer <mpalmer@hezmatt.org>
2013-04-21 04:46:42 +1000

    Remove overly verbose debug message
    DIE LOADING OBJECT, DIE

--- src/rdb.c
+++ src/rdb.c
@@ -913,7 +913,6 @@ robj *rdbLoadObject(int rdbtype, rio *rdb) {
     size_t len;
     unsigned int i;
 
-    redisLog(REDIS_DEBUG,"LOADING OBJECT %d (at %d)",rdbtype,rioTell(rdb));
     if (rdbtype == REDIS_RDB_TYPE_STRING) {
         /* Read string value */
         if ((o = rdbLoadEncodedStringObject(rdb)) == NULL) return NULL;
